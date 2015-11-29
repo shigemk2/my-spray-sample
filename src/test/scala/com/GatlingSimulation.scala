48065dfc6014a1e2cd98e76e1f1257ed71a2e15d
shigemk2 <i.am.shige@gmail.com>
2015-11-29 23:28:40 +0900

    ramdom number

--- src/test/scala/com/GatlingSimulation.scala
+++ src/test/scala/com/GatlingSimulation.scala
@@ -6,6 +6,6 @@ class GatlingSimulation extends Simulation {
   val req = http("Connection Test").get("http://127.0.0.1:8080").check(status.is(200))
   val scn = scenario("request").exec(req)
 
-  // setUp(scn.inject(ramp(1000 users) over (50 seconds)))
-  setUp(scn.inject(atOnceUsers(10000)))
+  setUp(scn.inject(rampUsers(10) over(5 seconds)))
+  // setUp(scn.inject(atOnceUsers(10000)))
 }
